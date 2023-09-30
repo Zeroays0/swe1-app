8ce835a3eeb27a3b7494d5b8e0b5dadb58869b98
Jimmy <jimmy@Jimmys-Air.lan>
2023-09-29 23:47:37 -0400

    Changed settings file for EB Environment Variable add-on

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"), verbose=True)
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '1').lower() in ['true', 't', '1']
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
