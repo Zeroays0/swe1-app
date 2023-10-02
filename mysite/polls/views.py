27345188138c9215e7a03e2871b28604887ea610
Jimmy <jimmy@Jimmys-Air.lan>
2023-10-02 12:18:24 -0400

    Adding default home page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -24,8 +24,12 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
 
+class HomeView(generic.base.TemplateView):
+    model = None
+    template_name = "polls/home.html"
 
-
+def index(request):
+    pass
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
