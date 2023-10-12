790f4e750bad0ce908ac5df47e65a8ee3a35cdc9
Jimmy <jimmy@Jimmys-Air.lan>
2023-10-12 13:04:33 -0400

    Changing SQLite in settings to 3.21

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,7 +83,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    }
+    },
+    "OPTIONS": {"timeout": 30, "version": "3.21"},
 }
 
 
